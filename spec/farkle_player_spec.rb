20e8630f060daceb6cd58f77c67f9d276775cd83
Mark Tareshawty <tarebyte@gmail.com>
2013-01-25 22:07:14 -0500

    initial spec file for farkle player

---
+++ spec/farkle_player_spec.rb
@@ -0,0 +1,30 @@
+# farkle_player.spec
+require_relative '../lib/farkle_player.rb'
+
+describe FarklePlayer do
+
+  it "starts a player on a new turn with a current score of 0" do
+    player = FarklePlayer.new "Mark"
+    player.start_turn
+    player.current_score.should eq(0)
+  end
+
+  it "starts a player out with 6 dice" do
+    player = FarklePlayer.new "Mark"
+    player.start_turn
+    player.current_dice_cup.length.should eq(6)
+  end
+
+  it "sets a value for every dice in the cup that is not 0" do
+    player = FarklePlayer.new "Mark"
+    player.start_turn
+    player.roll_dice
+    player.current_dice_cup.has_value?(0).should eq(false)
+  end
+
+  it "returns the given players name that was set" do
+    player = FarklePlayer.new "Mark"
+    player.name.should eq("Mark")
+  end
+  
+end
