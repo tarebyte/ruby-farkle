ce7d7031b5c46cbcf968baba0150976e14dc87dc
Mark Tareshawty <tarebyte@gmail.com>
2013-03-01 12:17:58 -0500

    reorganized an started to cleanup code

--- lib/ruby-farkle/farkle_moves.rb
+++ lib/ruby-farkle/farkle_moves.rb
@@ -1,56 +1,88 @@
 # ../lib/ruby-farkle/farkle_moves.rb
-
+#
+# Public: Methods that can be used by a farkle player in order for them
+# manipulate the dice to get the scoring of the choosing.
+#
+#
+# This class allows you to do the following:
+# 
+# For scoring
+# => Check the current set of dice and iterate a 'farkle count' if the set contains a farkle
+# ==> if the farkle count is 3 then the users current score gets set back to 0
+# 
+# For single scoring dice (1/5)
+# => 'Check' to see if there are any single scoring dice (returns true or false) 
+# => 'Collect' a single scoring dice and score accordingly
+# => 'Collect' all single scoring dice
+#
+# For sets of three
+# => 'Check' to see if there are any sets of three (returns true or false) 
+# => 'Collect' a single given set of three defined by the user and score accordingly
+# => 'Collect' all instances of sets of three and score them accordingly
+#
+ 
 class FarkleMoves
 
-  attr_accessor :current_score, :current_dice, :farkle_count, :total_score
+  attr_accessor :current_score, :current_dice, :dice_cup, :farkle_count, :total_score
 
   def initialize
 
-    @current_dice = {
-      1 => 0,
-      2 => 0,
-      3 => 0,
-      4 => 0,
-      5 => 0,
-      6 => 0
-    }
-
+    @dice_cup = Array.new
+    @number_of_dice = 6
     @current_score  = 0
     @farkle_count   = 0
     @total_score    = 0
 
   end
 
-  # Public: Remove all three of a kinds 
-  # and add them to the current score
+
+
+  #####################################################
+  ## Methods that do not deal with manipulating dice ##
+  #####################################################
+
+  # Public: Check is the user farkled, and if so 
+  # iterate the count by one
   #
-  # Returns nil if nothing was removed
+  # Return true is found otherwise false
+  def farkle?
+
+    if !self.sets_of_three? && !self.single_scoring?
+      @farkle_count += 1
+      @current_score = 0
+      true
+    else
+      false
+    end
+  end
+
+
+
+  ################################################
+  ## Methods that deal with single scoring dice ##
+  ################################################
+
+  # Public: Collect a single scoring die that the user chooses
   #
-  def collect_all_sets_of_three
+  def collect_one_die(value)
 
-    three_or_more = self.get_all_sets_of_three
+    if @dice_cup.has_value?(value)
 
-    if three_or_more != nil
+      if die == 1
+        @current_score += 100
+      else
+        @current_score += 50
+      end
 
-      three_or_more.each do |three_value|
-
-        # special case because three 1's get you 1000 points
-        if three_value == 1
-          @current_score += 1000
-        else
-          @current_score += ( three_value * 100 )
-        end
-
-        3.times do
-          @dice_cup.delete_if {|k, v| v == three_value }
-        end
-
+      key = 1
+      while @dice_cup.length != (@dice_cup.length - 1) do
+        @dice_cup.delete(key) if @dice_cup[key] == die
+        key += 1
       end
 
     else
       nil
     end
-
   end
 
   # Public: Pulls of all of the single scoring dice
@@ -60,7 +92,7 @@ class FarkleMoves
   # Each 1 gets you 100 points
   # Each 5 gets you  50 points
   #
-  def collect_all_single_scoring_dice
+  def collect_single_dice
 
     @dice_cup.each do |k, v|
 
@@ -81,92 +113,72 @@ class FarkleMoves
 
   end
 
+  # Public: Check the set to see if there
+  # are any single scoring dice
+  # 
+  # Return true if there are else false
+  #
+  def single_scoring?
+
+  end
+
+
+
+  ########################################################
+  ## Methods that deal with sets of three dice in a set ##
+  ########################################################
+
+  # Public: Remove all three of a kinds
+  # and add them to the current score
+  #
+  # Returns nil if nothing was removed
+  #
+  def collect_all_three_sets
+
+    three_or_more = self.get_all_sets_of_three
+
+    if three_or_more != nil
+
+      three_or_more.each do |three_value|
+
+        # special case because three 1's get you 1000 points
+        if three_value == 1
+          @current_score += 1000
+        else
+          @current_score += ( three_value * 100 )
+        end
+
+        3.times do
+          @dice_cup.delete_if {|k, v| v == three_value }
+        end
+      end
+
+    else
+      nil
+    end
+  end
+
   # Public: Collects and scores the what set of three
   # the user wants
   #
-  def collect_one_set_of_three ( value )
+  def collect_three_set(value)
 
+    new_set = @dice_cup.group_by{ |i| i }
 
-
-  end
-
-  # Public: Collect a single scoring die that the user chooses
-  #
-  def collect_one_scoring_die ( die )
-
-    if @dice_cup.has_value?(die)
-
-      if die == 1
-        @current_score += 100
-      else
-        @current_score += 50
-      end
-
-      key = 1
-      while @dice_cup.length != (@dice_cup.length - 1) do
-        @dice_cup.delete(key) if @dice_cup[key] == die
-        key += 1
-      end
-
-    else
-      nil
+    new_set.each_value do |v|
+      3.times { v.pop } if v.length >= 3 && v[0] == num
     end
 
+    @dice_cup.clear
+    new_set.each_value{ |v| self << v }
+    @dice_cup.flatten
   end
 
-  # Public: Check is the user farkled, and if so add 1 to there farkle count
-  #
-  # Return true is found otherwise false
-  def farkle?
-
-    if !self.are_there_any_three_or_more_sets? && !self.are_there_any_single_scoring_dice?
-      @farkle_count += 1
-      @current_score = 0
-      true
-    else
-      false
-    end
-
-  end
-
-  # Public: Tests if the player has 3 of a kind in their cup
-  #
-  # Returns all of the numbers that have three of a kind or more
-  # as a string or nil if there wasn't anything
-  #
-  def view_all_sets_of_three
-
-    three_of_a_kind_or_more = []
-
-    dice_point_total = {
-      1 => 0,
-      2 => 0,
-      3 => 0,
-      4 => 0,
-      5 => 0,
-      6 => 0
-    }
-
-    # Since the keys and the values are of the same type
-    # I can put in the value in place of the key value
-    # and increment by one
-    @dice_cup.each_value { |value| dice_point_total[value] += 1 }
-
-    dice_point_total.each { |k, v| three_of_a_kind_or_more << k if v >= 3 }
-
-    if three_of_a_kind_or_more.length > 0
-      three_of_a_kind_or_more.to_s
-    else
-      nil
-    end
-
-  end
-
-
-  # Public: Prints out all of the dice that can be used for single
-  # scoring that are not part of a set of three
-  #
-  def view_all_single_scoring_dice
+  # Public run a check to see if there is a set of three
+  # in the current dice collection
+  # 
+  # Return true else false
+  def three_set?
 
   end
 
