7096c061a67bc3381109d7640333a1274f052ce7
Mark Tareshawty <tarebyte@gmail.com>
2013-03-03 00:47:57 -0500

    finished basic player

--- lib/ruby-farkle/farkle_player.rb
+++ lib/ruby-farkle/farkle_player.rb
@@ -1,57 +1,39 @@
 # ../lib/ruby-farkle/farkle_player.rb
-require_relative 'farkle_moves.rb'
+require_relative 'farkle_brain.rb'
 
-class FarklePlayer < FarkleMoves
+class FarklePlayer < FarkleBrain
 
   # Public: Give every player attributes
   #
-  # Example
-  #
-  #  player1 = FarklePlayer.new "Mark"
-  #
-  def initialize( name )
+  def initialize(name)
     @name = name
   end
 
-  # Public: Lets the user know if there are sets of three
+  # Public: give the user their choices so that
+  # they can play the game
   #
-  def set_of_three?
-    self.get_all_sets_of_three != nil ? true : false
+  def options?
+    self.run_all_options      
   end
-
-  # Public: Returns whether there are any single scoring dice
-  #
-  def single_scoring_dice?
-
-    if !self.set_of_three?
-      ( @dice_cup.include?(1) || @dice_cup.include?(5) )
-    else
-      false
-    end
-
-  end
-
   # Public: List out all of the dice the user rolled
   #
-  def in_cup?
-    @dice_cup.each { |value| print "#{value} "}
+  def print_cup
+    self.to_s
   end
 
   # Public: For the number of dice the player has add newly
   # rolled dice to the cup
   #
   def roll_dice
-    self.number_of_dice.times { self.dice_cup << Random.rand(1..6) }
+    self.numer_of_dice.times { self.dice_cup << Random.rand(1..6) }
   end
 
   # Public: Start the player out with a clean slate
   # for their turn
   #
   def start_turn
-
-    self.current_score = 0
-    self.number_of_dice = 6
-    self.farkle_count = 0
+    self.reset
+    6.times { self.dice_cup << Random.rand(1..6) }
   end
 
 end
